2ad797bbc85c5bdfc6c562c8df9cfa9a2d7f2f60
regis.titus <titus.regis@gmail.com>
2017-12-03 19:14:34 +0100

    [VENDOR] Handle account management

--- SRC/Backend/Vendor/connection_vendor.php
+++ SRC/Backend/Vendor/connection_vendor.php
@@ -9,7 +9,7 @@
     {
         $mail = $_POST['login'];
         $password = $_POST['password'];
-        $request = $pdo->prepare("SELECT u.id as user_id, s.id as shop_id FROM user u JOIN shop s ON u.id = s.owner_id WHERE u.mail = ?");
+        $request = $pdo->prepare("SELECT u.id as user_id, s.id as shop_id FROM user u JOIN shop s ON u.id = s.owner_id WHERE u.email = ?");
         $request->bindParam(1, $mail, PDO::PARAM_STR);
         $request->execute();
         if ($request->rowCount()  == 1)
