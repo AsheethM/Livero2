16d5af6f5e0628057616a7323d7f2d28e1ef9676
regis.titus <titus.regis@gmail.com>
2017-12-12 00:57:09 +0100

    Todo Sql updated

--- SRC/pri.sql
+++ SRC/pri.sql
@@ -9,17 +9,17 @@ CREATE TABLE IF NOT EXISTS user
   birthdate DATE NOT NULL ,
   phone VARCHAR(10) NOT NULL,
   phone_token VARCHAR(255),
-  role INT NOT NULL COMMENT ' 1: customer | 2:vendor | 4:deliverer | 3:customer-vendor | 5: customer-deliverer | 6: vendor-deliverer | 7 : all roles',
+  role INT NOT NULL COMMENT ' 1: customer | 2:shop | 4:deliverer | 3:customer-shop | 5: customer-deliverer | 6: shop-deliverer | 7 : all roles',
   creation_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   lastlogin TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS vendor
+CREATE TABLE IF NOT EXISTS shop
 (
   id INT NOT NULL AUTO_INCREMENT,
   owner_id INT NOT NULL,
-  vendor_name VARCHAR(255) NOT NULL,
+  shop_name VARCHAR(255) NOT NULL,
   address VARCHAR(255) NOT NULL ,
   latitude VARCHAR(255) NOT NULL,
   longitude VARCHAR(255) NOT NULL,
@@ -31,15 +31,15 @@ CREATE TABLE IF NOT EXISTS vendor
 
 
 
-CREATE TABLE IF NOT EXISTS vendor_schedule
+CREATE TABLE IF NOT EXISTS shop_schedule
 (
   id INT NOT NULL AUTO_INCREMENT,
-  vendor_id INT NOT NULL ,
+  shop_id INT NOT NULL ,
   day VARCHAR(15) NOT NULL ,
   opening_hour TIME NOT NULL ,
   closing_hour TIME NOT NULL ,
   PRIMARY KEY (id),
-  FOREIGN KEY (vendor_id) REFERENCES vendor(id)
+  FOREIGN KEY (shop_id) REFERENCES shop(id)
 );
 
 
@@ -47,11 +47,11 @@ CREATE TABLE IF NOT EXISTS product
 (
   id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(255) NOT NULL ,
-  vendor_id INT NOT NULL ,
+  shop_id INT NOT NULL ,
   description VARCHAR(255) NOT NULL DEFAULT '',
-  price INT NOT NULL ,
+  price DOUBLE NOT NULL ,
   PRIMARY KEY (id),
-  FOREIGN KEY (vendor_id) REFERENCES vendor(id)
+  FOREIGN KEY (shop_id) REFERENCES shop(id)
 );
 
 CREATE TABLE IF NOT EXISTS transaction
@@ -62,17 +62,17 @@ CREATE TABLE IF NOT EXISTS transaction
   client_long VARCHAR(255) NOT NULL,
   dest_address VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL COMMENT 'Message of the client when he creates the transaction',
-  vendor_id INT NOT NULL,
+  shop_id INT NOT NULL,
   deliverer_id INT,
   client_token VARCHAR(12) NOT NULL DEFAULT '',
-  vendor_token VARCHAR(12) NOT NULL DEFAULT '',
+  shop_token VARCHAR(12) NOT NULL DEFAULT '',
   order_price DOUBLE NOT NULL ,
   deliverer_price DOUBLE,
-  status INT NOT NULL COMMENT '1: Creation of the transaction, 2: Confirmation of Vendor, 3: Confirmation of Client, 4: Transaction Finished',
+  status INT NOT NULL COMMENT '1: Creation of the transaction, 2: Confirmation of shop, 3: Confirmation of Client, 4: Transaction Finished',
   isComplete BOOLEAN DEFAULT true,
   PRIMARY KEY (id),
   FOREIGN KEY (client_id) REFERENCES user(id),
-  FOREIGN KEY (vendor_id) REFERENCES vendor(id),
+  FOREIGN KEY (shop_id) REFERENCES shop(id),
   FOREIGN KEY (deliverer_id) REFERENCES user(id)
 );
 
@@ -100,7 +100,7 @@ CREATE TABLE IF NOT EXISTS transaction_product
   product_id INt NOT NULL,
   transaction_id INT NOT NULL,
   customer_quantity INT NOT NULL,
-  vendor_quantity INT NOT NULL ,
+  shop_quantity INT NOT NULL ,
   PRIMARY KEY (id),
   FOREIGN KEY (product_id) REFERENCES product(id),
   FOREIGN KEY (transaction_id) REFERENCES transaction(id)
