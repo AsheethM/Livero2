58b1a2884feebfb45741deadd687e78a60584be4
Nicolas Pelletier <nicolas.pelletier@epita.fr>
2017-12-21 19:09:55 +0100

    get the SRC_QRCODE_PRI

---
+++ SRC_PRI_QRCODE/QRCode/get_client_token.php
@@ -0,0 +1,40 @@
+<?php
+
+    header('Content-Type: application/json');
+    $response = array();
+    $success =  false;
+    $message  = "";
+
+    if (isset($_POST['transaction_id']) && !empty($_POST['transaction_id'])
+        && isset($_POST['client_id']) && !empty($_POST['client_id']))
+    {
+        $transaction_id = $_POST['transaction_id'];
+        $user_id = $_POST['client_id'];
+
+        $req_str = 'SELECT client_token FROM TRANSACTION WHERE id = ? AND customer_id = ?';
+
+        require_once('../connexion.php');
+        $request = $pdo->prepare($req_str);
+        $request->bindParam(1, $transaction_id, PDO::PARAM_INT);
+        $request->bindParam(2, $client_id, PDO::PARAM_INT);
+        $request->execute();
+
+        if ($request->rowCount() > 0)
+        {
+            $success = true;
+            $response['results'] = $request->fetchAll();
+            $message = "Request Get Client Token : OK";
+        }
+        else
+            $message = "BDD Error";
+    }
+    else
+        $message = "Parameters Error";
+
+
+
+    $response['success'] = $success;
+    $response['message'] = $message;
+
+    echo json_encode($response);
+?>
